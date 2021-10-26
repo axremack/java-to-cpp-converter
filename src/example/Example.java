8e7162bd689de8ee5ee5843334dbd9357dc7904f
uke <ax.remack@gmail.com>
2021-10-26 09:35:51 +0200

    :construction: Getting class name and attribute infos from String

--- src/example/Example.java
+++ src/example/Example.java
@@ -1,5 +1,37 @@
 package example;
 
 public class Example {
+    // ------------------------------------
+    // Getting as many cases as possible
+    //      - Diverse types
+    //      - Diverse access
+    //      - Static attributes / methods
+    // ------------------------------------
 
+    // Attributes
+    private String name;
+    private double grade;
+    public static int cpt = 0;
+
+    // Methods
+    public Example(String name, double grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,26 @@
 package main;
 
+import example.Example;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello");
+        String class_name = "example.Example";
+        Class c = Class.forName(class_name);
+        System.out.println(c.getName());
+
+        // Getting attributes name, access and type
+        Field[] classFields = c.getDeclaredFields();
+        for (Field field : classFields) {
+            System.out.print("Field : ");
+            System.out.print("type - " + field.getType());
+            System.out.println(" // name and access - " + field.toGenericString());
+        }
+
+
+
     }
 }
