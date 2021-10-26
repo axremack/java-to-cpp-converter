dc3cfd7d86e0a9fb4791b93593ec0a5a782435db
uke <ax.remack@gmail.com>
2021-10-26 09:55:16 +0200

    :construction: Getting signature of methods from String

--- src/example/Example.java
+++ src/example/Example.java
@@ -1,5 +1,7 @@
 package example;
 
+import java.util.List;
+
 public class Example {
     // ------------------------------------
     // Getting as many cases as possible
@@ -11,12 +13,14 @@ public class Example {
     // Attributes
     private String name;
     private double grade;
+    private List<Float> stats;
     public static int cpt = 0;
 
     // Methods
-    public Example(String name, double grade) {
+    public Example(String name, double grade, List<Float> stats) {
         this.name = name;
         this.grade = grade;
+        this.stats = stats;
     }
 
     public String getName() {
@@ -33,5 +37,13 @@ public class Example {
 
     public void setGrade(double grade) {
         this.grade = grade;
+    }
+
+    public List<Float> getStats() {
+        return stats;
+    }
+
+    public void setStats(List<Float> stats) {
+        this.stats = stats;
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,7 @@ package main;
 import example.Example;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,14 +13,27 @@ public class Main {
         Class c = Class.forName(class_name);
         System.out.println(c.getName());
 
-        // Getting attributes name, access and type
+        // Getting attributes infos
         Field[] classFields = c.getDeclaredFields();
         for (Field field : classFields) {
             System.out.print("Field : ");
-            System.out.print("type - " + field.getType());
-            System.out.println(" // name and access - " + field.toGenericString());
+            String[] fieldString = field.toGenericString().split(" ");
+            System.out.print("access - " + fieldString[0]);
+            System.out.print(" // type - " + fieldString[1]);
+            System.out.println(" // name - " + fieldString[2]);
         }
+        System.out.println();
 
+        // Getting methods signatures
+        Method[] classMethods = c.getDeclaredMethods();
+        for (Method method : classMethods) {
+            System.out.print("Method : ");
+            String[] methodString = method.toGenericString().split(" ");
+            System.out.print("access - " + methodString[0]);
+            System.out.print(" // return type - " + methodString[1]);
+            System.out.println(" // name and args - " + methodString[2]);
+        }
+        System.out.println();
 
 
     }
